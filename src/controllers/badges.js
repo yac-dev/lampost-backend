910e681a9a0644868220577a41661450257f0557
kojima yosuke <yabbee0828@gmail.com>
2023-02-19 16:18:17 +0900

    Updated filter query

--- src/controllers/badges.js
+++ src/controllers/badges.js
@@ -6,48 +6,38 @@ import Asset from '../models/asset';
 export const getBadges = async (request, response) => {
   try {
     // const filteringUserBadges = [];
-    let badges = Badge.find({}); // grab all
-    if (request.body.userId) {
-      const badgeAndUserRelationships = await BadgeAndUserRelationship.find({ user: request.body.userId }).populate({
-        path: 'badge',
-      });
-      // const user = await User.findById(request.body.userId).populate({
-      //   path: 'badges',
-      //   model: BadgeStatus,
-      //   populate: {
-      //     path: 'badge',
-      //     model: Badge,
-      //   },
-      // });
+    const { filterOption } = request.body;
 
-      const filteringUserBadges = badgeAndUserRelationships.map((element) => element.badge._id);
-      // for (let i = 0; i < user.badges.length; i++) {
-      //   console.log(user.badges[i]);
-      //   filteringUserBadges.push(user.badges[i].badge._id);
-      // }
-      badges.where({ _id: { $nin: filteringUserBadges } });
-    }
+    let badges = await Badge.find({ type: filterOption }); // grab all
+    // if (request.body.userId) {
+    //   const badgeAndUserRelationships = await BadgeAndUserRelationship.find({ user: request.body.userId }).populate({
+    //     path: 'badge',
+    //   });
 
-    let queryFilters = [];
+    //   const filteringUserBadges = badgeAndUserRelationships.map((element) => element.badge._id);
+    //   badges.where({ _id: { $nin: filteringUserBadges } });
+    // }
 
-    if (request.query.type) {
-      const queryByType = { type: request.query.type };
-      queryFilters.push(queryByType);
-    }
-    if (request.query.name) {
-      const queryByName = { name: request.query.name };
-      queryFilters.push(queryByName);
-    }
+    // let queryFilters = [];
 
-    if (queryFilters.length) {
-      badges = badges.where({ $and: queryFilters });
-    }
+    // if (request.query.type) {
+    //   const queryByType = { type: request.query.type };
+    //   queryFilters.push(queryByType);
+    // }
+    // if (request.query.name) {
+    //   const queryByName = { name: request.query.name };
+    //   queryFilters.push(queryByName);
+    // }
 
-    const limit = 600;
-    const page = request.query.page;
-    const skip = (page - 1) * limit;
+    // if (queryFilters.length) {
+    //   badges = badges.where({ $and: queryFilters });
+    // }
 
-    badges = await badges.skip(skip).limit(limit);
+    // const limit = 600;
+    // const page = request.query.page;
+    // const skip = (page - 1) * limit;
+
+    // badges = await badges.skip(skip).limit(limit);
 
     response.status(200).json({
       badges,
